d87a169032d337e51b84830b2c7f82a9143a511a
Dominic Tarr <dominic.tarr@gmail.com>
2018-03-04 22:27:04 +1300

    switch to new avatar api

--- message.js
+++ message.js
@@ -11,6 +11,25 @@ exports.gives = {
 
 exports.create = function (api) {
   function noop () {}
+//  var int = setInterval(function () {
+//    var a = document.querySelectorAll('.timestamp')
+//    for(var i = 0; i < a.length; i++) {
+//      var _ago = ago(Date.now(), +a[i].dataset.timestamp)
+//      if(a[i].textContent != _ago) a[i].textContent = _ago
+//    }
+//  }, 10000)
+//
+//  if(int.unref) int.unref()
+//
+
+  function toImg (fn) {
+    return function (opts) {
+      var img = h('img')
+      fn(opts, function (url) { img.src = url })
+      return img
+    }
+  }
+
   return {
     message: {
       //TODO: implement a "optional" thing for depject so don't get errors when
@@ -23,14 +42,12 @@ exports.create = function (api) {
         return h('div.message', [
           h('div.side',
             h('div.Avatar',
-              h('a', {href: data.value.author},
-                api.avatar.image(data.value.author)
-              ),
+              h('a', {href: data.value.author}, toImg(api.avatar.image)(data.value.author)),
               h('a', {href: data.value.author},
                 api.avatar.name(data.value.author)
               )
             ),
-            h('a', {href: data.key}, ago(Date.now(), data.value.timestamp))
+            h('a.timestamp', {href: data.key, 'data-timestamp': data.value.timestamp}, ago(Date.now(), data.value.timestamp))
           ),
           h('div.message__wrapper',
             h('div.MessageMeta', api.message.meta(data, context)),
